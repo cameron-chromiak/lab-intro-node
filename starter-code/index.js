8048cffedff3947234a3255a79b4e2bf864567b0
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-11 11:01:52 -0500

    7 passes

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,37 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(){
+    this.items = [];
+    this.length = this.items.length
+  }
+  add(item){
+    this.items.push(item)
+  }
+  get(pos){
+    return this.items[pos]
+  }
+  max(){
+    if(this.length > 0){
+      return max(this.items)
+    }else{
+      throw new Error("EmptySortedList")
+    }
+
+  }
+  min(){
+    if(this.length > 0){
+      return min(this.items)
+    }else{
+      throw new Error("EmptySortedList")
+    }
+  }
+  average(){
+    let reducer = (a, b)=> a + b;
+    return this.items.reduce(reducer)/ this.items.length
+  }
+  sum(){
+    let reducer = (a, b)=> a + b;
+    return this.items.reduce(reducer)
+  }
 };
 
 module.exports = SortedList;
